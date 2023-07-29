e65d13061926fb3fff4063e7994043592597a64f
faun3 <antonioculcea@gmail.com>
2023-07-29 18:51:11 +0300

    test some tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Tailwind</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-500 text-white-900 text-center my-6 w-full h-[10rem] flex justify-center items-center">
+      Hello Tailwind
+    </h1>
+  );
 }
 
 export default App;
