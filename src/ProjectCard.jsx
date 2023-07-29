89de28871aa7354a46f5a3d8dcfcef0851de2ad6
faun3 <antonioculcea@gmail.com>
2023-07-29 21:33:04 +0300

    work on placing image in card component

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,11 +1,14 @@
-const ProjectCard = ({ big }) => {
+const ProjectCard = ({ big, img, copy }) => {
   return (
     <div
-      className={`bg-emerald-500 rounded-lg h-[20rem] w-[15rem] ${
+      className={`bg-emerald-500 rounded-lg h-[30rem] w-[15rem] ${
         big ? "basis-2/3" : "basis-1/2"
-      }`}
+      } relative overflow-hidden`}
     >
-      <p>Copy text here</p>
+      <p className="text-white text-2xl font-bold text-center mt-10">{copy}</p>
+      <div className="rounded-xl overflow-hidden absolute bottom-[10%] w-[80%] left-[50%] translate-x-[-50%]">
+        <img src={img} alt="" srcset="" />
+      </div>
     </div>
   );
 };
