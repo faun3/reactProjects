f52e75696dbb1e55c23544140fc7ee13d4d7cfae
faun3 <antonioculcea@gmail.com>
2023-07-29 20:29:32 +0300

    add basic styling to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <nav className="w-[50%] mx-auto my-10">
-      <ul className="flex w-full gap-5 justify-between">
-        <li>
+    <nav className="md:w-[50%] sm:w-full mx-auto my-10 rounded-full bg-slate-700 py-6 px-4">
+      <ul className="flex w-full gap-5 justify-between px-4">
+        <li className="hover:bg-slate-200">
           <a href="">Projects</a>
         </li>
         <li>
