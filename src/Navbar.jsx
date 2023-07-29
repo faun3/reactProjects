531126a04d98ae5bff14bc7d3ce24f37045574c3
faun3 <antonioculcea@gmail.com>
2023-07-29 20:24:09 +0300

    add Navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav className="w-[50%] mx-auto my-10">
+      <ul className="flex w-full gap-5 justify-between">
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
