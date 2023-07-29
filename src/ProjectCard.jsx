95288a64b00e9c3f426e332ae34121778a09ef32
faun3 <antonioculcea@gmail.com>
2023-07-30 00:33:07 +0300

    tweak image sizing and positioning

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -14,15 +14,15 @@ const ProjectCard = ({ big, img, copy }) => {
 
   return (
     <div
-      className={`bg-emerald-500 rounded-lg h-[30rem] w-[15rem] ${
-        big ? "basis-2/3" : "basis-1/2"
-      } relative overflow-hidden sm:basis-full`}
+      className={`bg-emerald-500 rounded-lg ${
+        big ? "basis-2/3" : "basis-1/3"
+      } relative overflow-hidden basis-full sm:min-w-[300px] min-h-[25rem]`}
       onMouseEnter={toggleDetes}
       onMouseLeave={hideDetes}
     >
-      <p className="text-white text-2xl font-bold text-center mt-10">{copy}</p>
-      <div className="rounded-xl overflow-hidden absolute bottom-[10%] w-[80%] left-[50%] translate-x-[-50%]">
-        <img src={img} alt="" srcset="" />
+      <p className="text-white text-2xl font-bold text-center my-10">{copy}</p>
+      <div className="rounded-xl overflow-hidden absolute bottom-[0rem] w-[70%] h-[50%] left-[50%] translate-x-[-50%]">
+        <img src={img} alt="" srcset="" className="w-full h-full" />
       </div>
       <div
         className={`absolute w-full h-full top-0 left-0 ${
