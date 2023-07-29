aee6e39dc7d93ff2acc392b2ae6eaab6069c0836
faun3 <antonioculcea@gmail.com>
2023-07-29 20:13:41 +0300

    work on basic Namer styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Namer from "./Namer";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-500 text-white-900 text-center my-6 w-full h-[10rem] flex justify-center items-center">
-      Hello Tailwind
-    </h1>
+    <div className="bg-slate-600">
+      <Namer />
+    </div>
   );
 }
 
