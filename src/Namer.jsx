cb0778638997c6ff7ca67a73581465dda167512d
faun3 <antonioculcea@gmail.com>
2023-07-29 21:09:04 +0300

    change title text and add description copy

--- src/Namer.jsx
+++ src/Namer.jsx
@@ -2,17 +2,25 @@ import galaxy from "./assets/galaxy.svg";
 
 const Namer = () => {
   return (
-    <div className="w-full relative">
-      <h1 className="text-5xl text-center font-bold z-[2] text-slate-900 py-[12rem] relative">
-        Hello. This is xyz
-      </h1>
-      <img
-        alt=""
-        src={galaxy}
-        srcset=""
-        className="absolute top-[50%] left-[50%] -translate-x-[50%] translate-y-[-50%] z-[1] fill-slate-300 stroke-slate-500 opacity-20 w-[150%] h-[100%] "
-      />
-    </div>
+    <>
+      <div className="w-full relative">
+        <h1 className="text-6xl text-center font-bold z-[2] text-slate-900 py-[12rem] relative">
+          Hi, I'm [name]. <br />I make web apps and art.
+        </h1>
+        <img
+          alt=""
+          src={galaxy}
+          srcset=""
+          className="absolute top-[50%] left-[50%] -translate-x-[50%] translate-y-[-50%] z-[1] fill-slate-300 stroke-slate-500 opacity-20 w-[150%] h-[100%] "
+        />
+      </div>
+      <div className="my-5 mx-10">
+        <p className="text-center text-3xl font-bold w-[60%] mx-auto text-slate-700">
+          I am passionate about working on web apps that are a joy to use and to
+          look at.
+        </p>
+      </div>
+    </>
   );
 };
 export default Namer;
