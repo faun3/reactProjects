2f31d900b9502c4916c0df2bcc392a1f5a914145
faun3 <antonioculcea@gmail.com>
2023-07-29 23:39:39 +0300

    made nav sticky and adjusted blur and color

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="md:w-[50%] sm:w-full mx-auto my-10 rounded-full py-6 px-4 bg-[rgba(255,255,255,0.15)] backdrop-blur-sm z-[100]">
+    <nav className="md:w-[50%] sm:w-full mx-auto my-10 rounded-full py-6 px-4 bg-[rgba(255,255,255,0.25)] backdrop-blur-md z-[100] sticky top-5">
       <ul className="flex w-full gap-5 justify-between px-4">
         <li className="hover:">
           <a href="">Projects</a>
