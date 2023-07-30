227c9bf4cfd02e5dd4cb5584ef8ebebd8202ead3
faun3 <antonioculcea@gmail.com>
2023-07-30 22:30:28 +0300

    work on styling active navbar element

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,24 @@
 const Navbar = () => {
   return (
-    <nav className="md:w-[60%] w-[90%] md:mx-auto my-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.25)] backdrop-blur-md z-[100] sticky top-5">
+    <nav className="md:w-[60%] w-[90%] md:mx-auto my-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.35)] backdrop-blur-lg z-[100] sticky top-5">
       <ul className="flex w-full gap-5 justify-between px-4">
         <li className="hover:">
-          <a href="">Projects</a>
+          <a
+            href=""
+            className="font-bold text-xl text-slate-900 rounded-full p-3 border-slate-700 border-[2px]"
+          >
+            Projects
+          </a>
         </li>
         <li>
-          <a href="">About</a>
+          <a href="" className="font-bold text-xl text-slate-700">
+            About
+          </a>
         </li>
         <li>
-          <a href="">Contact</a>
+          <a href="" className="font-bold text-xl text-slate-700">
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
