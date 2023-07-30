aff8df01ad06f059f45cbdce39c986751a43bcd5
faun3 <antonioculcea@gmail.com>
2023-07-30 23:16:54 +0300

    fix color declaration in project cards

--- src/Namer.jsx
+++ src/Namer.jsx
@@ -13,12 +13,13 @@ const Namer = () => {
         </p>
       </div>
       <div className="my-10">
-        <p className="text-3xl font-bold mt-20 text-center text-emerald-800">
+        <p className="text-3xl font-bold mt-20 text-center text-slate-800">
           A sneak peek of what I've worked on:
         </p>
         <div className="flex md:flex-row flex-col gap-10 items-center flex-wrap md:flex-nowrap justify-center my-10 px-10 ll">
-          <ProjectCard copy={`sorry nothing`} img={img1} />
-          <ProjectCard big={true} img={img1} copy={`This image is stolen!`} />
+          <ProjectCard copy={`sorry nothing`} img={img1} color={"emerald"} />
+          <ProjectCard copy={`sorry nothing`} img={img1} color={"pink"} />
+          <ProjectCard copy={`sorry nothing`} img={img1} color={"cyan"} />
         </div>
       </div>
     </>
