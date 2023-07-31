8016c9d8af1f495ca99858ce84dea4f8936b2371
faun3 <antonioculcea@gmail.com>
2023-07-31 19:16:40 +0300

    add routing to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import About from "./routes/About.jsx";
+import Contact from "./routes/Contact.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+} from "react-router-dom";
+import Namer from "./Namer.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+    </Routes>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
