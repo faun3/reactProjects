5bbb27bb618aeec15d40c2948c89e36ea1042567
faun3 <antonioculcea@gmail.com>
2023-07-31 21:02:19 +0300

    add gradient background to all route pages

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,10 +3,10 @@ import SectionIntro from "../SectionIntro";
 
 const About = () => {
   return (
-    <>
+    <div className="bg-gradient-to-b from-slate-400 to-slate-200">
       <Navbar></Navbar>
       <SectionIntro text={`All about me`}></SectionIntro>
-    </>
+    </div>
   );
 };
 export default About;
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -3,10 +3,10 @@ import SectionIntro from "../SectionIntro";
 
 const Contact = () => {
   return (
-    <>
+    <div className="bg-gradient-to-b from-slate-400 to-slate-200">
       <Navbar />
       <SectionIntro text={`Let's get in touch`}></SectionIntro>
-    </>
+    </div>
   );
 };
 export default Contact;
