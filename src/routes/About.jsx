e3a42423b8093e6999ec062bea1f96503ba2401a
faun3 <antonioculcea@gmail.com>
2023-07-31 19:43:46 +0300

    added Navbar to all route pages

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,4 +1,11 @@
+import Navbar from "../Navbar";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>About</div>
+    </>
+  );
 };
 export default About;
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,4 +1,11 @@
+import Navbar from "../Navbar";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Navbar />
+      <div>Contact</div>
+    </>
+  );
 };
 export default Contact;
