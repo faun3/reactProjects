0aed1df0e15e733bc71c11d262549a24f882a5d2
faun3 <antonioculcea@gmail.com>
2023-07-31 21:01:22 +0300

    remove top padding

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="md:w-[60%] w-[90%] md:mx-auto my-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.35)] backdrop-blur-lg z-[100] sticky top-5">
+    <nav className="md:w-[60%] w-[90%] md:mx-auto mb-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.35)] backdrop-blur-lg z-[100] sticky top-5">
       <ul className="flex w-full gap-5 justify-between px-4">
         <li className="hover:">
           <Link
