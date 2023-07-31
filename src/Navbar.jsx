3283f034c4b48c7ca076fa47a8f5948ec032ae1b
faun3 <antonioculcea@gmail.com>
2023-07-31 19:31:58 +0300

    change a tags to Link to make routing work

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="md:w-[60%] w-[90%] md:mx-auto my-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.35)] backdrop-blur-lg z-[100] sticky top-5">
       <ul className="flex w-full gap-5 justify-between px-4">
         <li className="hover:">
-          <a
-            href=""
+          <Link
+            to={`/`}
             className="font-bold md:text-xl text-slate-900 rounded-full p-3 border-slate-700 border-[2px]"
           >
             Projects
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="" className="font-bold md:text-xl text-slate-700">
+          <Link to={"/about"} className="font-bold md:text-xl text-slate-700">
             About
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="" className="font-bold md:text-xl text-slate-700">
+          <Link to={"/contact"} className="font-bold md:text-xl text-slate-700">
             Contact
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
