d1c479429e488726316b9b365a02a892f3ec085f
faun3 <antonioculcea@gmail.com>
2023-07-31 21:21:52 +0300

    factor out selected link style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,26 +1,29 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="md:w-[60%] w-[90%] md:mx-auto mb-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.35)] backdrop-blur-lg z-[100] sticky top-5">
       <ul className="flex w-full gap-5 justify-between px-4">
         <li className="hover:">
-          <Link
-            to={`/`}
-            className="font-bold md:text-xl text-slate-900 rounded-full p-3 border-slate-700 border-[2px]"
-          >
+          <NavLink to={`/`} className="font-bold md:text-xl text-slate-900 ">
             Projects
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to={"/about"} className="font-bold md:text-xl text-slate-700">
+          <NavLink
+            to={"/about"}
+            className="font-bold md:text-xl text-slate-700"
+          >
             About
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to={"/contact"} className="font-bold md:text-xl text-slate-700">
+          <NavLink
+            to={"/contact"}
+            className="font-bold md:text-xl text-slate-700"
+          >
             Contact
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
