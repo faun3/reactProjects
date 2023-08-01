23d6c62296d3ccbcd8b86b1d2b76994fb21a187e
faun3 <antonioculcea@gmail.com>
2023-08-01 19:19:50 +0300

    fix font color

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="md:w-[60%] w-[90%] md:mx-auto mb-10 mx-auto rounded-full py-6 px-4 bg-[rgba(255,255,255,0.35)] backdrop-blur-lg z-[100] sticky top-5">
       <ul className="flex w-full gap-5 justify-between px-4">
         <li className="hover:">
-          <NavLink to={`/`} className="font-bold md:text-xl text-slate-900 ">
+          <NavLink to={`/`} className="font-bold md:text-xl text-slate-700 ">
             Projects
           </NavLink>
         </li>
